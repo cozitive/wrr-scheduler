5672a127f4b08941883ffb36f0952acb18f485c7
Subin Song <lemonshushu@lemonshushu.me>
2023-05-15 20:31:01 +0900

    replace cfs trigger_load_balance() with triger_load_balance_wrr()

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3059,7 +3059,7 @@ void scheduler_tick(void)
 
 #ifdef CONFIG_SMP
 	rq->idle_balance = idle_cpu(cpu);
-	trigger_load_balance(rq);
+	trigger_load_balance_wrr(rq); // LB_TODO : Add synchronization
 #endif
 }
 
