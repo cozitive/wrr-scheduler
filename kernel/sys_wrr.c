40e886f6bd4d013dbd0b508463a762a15ad0f7c1
Hyein Kim <cozitive.dev@gmail.com>
2023-05-20 18:49:47 +0900

    add permission error handling to syscall

--- kernel/sys_wrr.c
+++ kernel/sys_wrr.c
@@ -1,3 +1,4 @@
+#include <linux/cred.h>
 #include <linux/kernel.h>
 #include <linux/smp.h>
 #include <linux/syscalls.h>
@@ -6,28 +7,32 @@
 
 SYSCALL_DEFINE2(sched_setweight, pid_t, pid, unsigned int, weight)
 {
-	// WRR_TODO: add permission error handling
 	struct task_struct *p;
 	struct sched_wrr_entity *wrr_se;
 	struct wrr_rq *wrr_rq;
 	unsigned int old_weight;
 
-	p = find_task_by_vpid(pid);
 	if (pid < 0) {
-		printk(KERN_INFO "sched_setweight: pid should be positive");
+		printk(KERN_DEBUG "sched_setweight: pid should be positive");
 		return -EINVAL;
 	}
 	if (weight < 1 || weight > 20) {
-		printk(KERN_INFO
+		printk(KERN_DEBUG
 		       "sched_setweight: weight should be between 1 and 20");
 		return -EINVAL;
 	}
+	// WRR_TODO: -EINVAL if the task with the given PID is not under the SCHED_WRR policy.
+
+	p = find_task_by_vpid(pid);
 	if (p == NULL) {
-		printk(KERN_INFO "sched_setweight: process not found");
+		printk(KERN_DEBUG "sched_setweight: process not found");
 		return -ESRCH;
 	}
 
-	// WRR_TODO: -EINVAL if the task with the given PID is not under the SCHED_WRR policy.
+	if (((int)current_cred()->uid.val != 0) && (current->pid != p->pid)) {
+		printk(KERN_DEBUG "sched_setweight: no permission to process");
+		return -EPERM;
+	}
 
 	wrr_se = &p->wrr;
 	wrr_rq = &task_rq(p)->wrr;
@@ -42,20 +47,19 @@ SYSCALL_DEFINE2(sched_setweight, pid_t, pid, unsigned int, weight)
 
 SYSCALL_DEFINE1(sched_getweight, pid_t, pid)
 {
-	// WRR_TODO: add permission error handling
 	struct task_struct *p;
 
-	p = find_task_by_vpid(pid);
 	if (pid < 0) {
-		printk(KERN_INFO "sched_getweight: pid should be positive");
+		printk(KERN_DEBUG "sched_getweight: pid should be positive");
 		return -EINVAL;
 	}
+	// WRR_TODO: -EINVAL if the task with the given PID is not under the SCHED_WRR policy.
+
+	p = find_task_by_vpid(pid);
 	if (p == NULL) {
-		printk(KERN_INFO "sched_getweight: process not found");
+		printk(KERN_DEBUG "sched_getweight: process not found");
 		return -ESRCH;
 	}
-
-	// WRR_TODO: -EINVAL if the task with the given PID is not under the SCHED_WRR policy.
 
 	return p->wrr.weight;
 }
