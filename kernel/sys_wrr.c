4d748da54726a74a97311b8022de51b6ef8a8ac6
Hyein Kim <cozitive.dev@gmail.com>
2023-05-21 18:01:50 +0900

    fix EPERM check + init_task handling

--- kernel/sys_wrr.c
+++ kernel/sys_wrr.c
@@ -7,45 +7,50 @@
 
 SYSCALL_DEFINE2(sched_setweight, pid_t, pid, unsigned int, weight)
 {
+	unsigned int uid;
 	struct task_struct *p;
 	struct sched_wrr_entity *wrr_se;
 	struct wrr_rq *wrr_rq;
-	unsigned int old_weight;
+	unsigned int weight_diff;
 
+	// pid must be positive
 	if (pid < 0) {
-		printk(KERN_DEBUG "sched_setweight: pid should be positive");
 		return -EINVAL;
 	}
 
+	// weight must be in valid range [1, 20]
 	if (weight < 1 || weight > 20) {
-		printk(KERN_DEBUG
-		       "sched_setweight: weight should be between 1 and 20");
 		return -EINVAL;
 	}
 
-	p = find_task_by_vpid(pid);
+	// find task with the given pid
+	p = (pid != 0) ? find_task_by_vpid(pid) : &init_task;
 	if (p == NULL) {
-		printk(KERN_DEBUG "sched_setweight: process not found");
 		return -ESRCH;
 	}
 
+	// task's scheduling policy must be WRR
 	if (p->policy != SCHED_WRR) {
-		printk(KERN_DEBUG "sched_getweight: process policy is not SCHED_WRR");
 		return -EINVAL;
 	}
 
-	if (((int)current_cred()->uid.val != 0) && (current->pid != p->pid)) {
-		printk(KERN_DEBUG "sched_setweight: no permission to process");
+	// only administrator or task owner can set weight
+	uid = (unsigned int)current_cred()->uid.val;
+	if ((uid != 0) && (uid != p->cred->uid.val)) {
 		return -EPERM;
 	}
 
+	// only administrator can increase weight
 	wrr_se = &p->wrr;
-	wrr_rq = &task_rq(p)->wrr;
+	weight_diff = weight - wrr_se->weight;
+	if ((uid != 0) && (weight_diff > 0)) {
+		return -EPERM;
+	}
 
-	old_weight = wrr_se->weight;
-	wrr_se->weight = weight;
-	wrr_rq->total_weight += (weight - old_weight);
-	WARN_ON(wrr_rq->total_weight < 0);
+	// change weight
+	wrr_rq = &task_rq(p)->wrr;
+	wrr_se->weight += weight_diff;
+	wrr_rq->total_weight += weight_diff;
 
 	return 0;
 }
@@ -54,19 +59,19 @@ SYSCALL_DEFINE1(sched_getweight, pid_t, pid)
 {
 	struct task_struct *p;
 
+	// pid must be positive
 	if (pid < 0) {
-		printk(KERN_DEBUG "sched_getweight: pid should be positive");
 		return -EINVAL;
 	}
 
-	p = find_task_by_vpid(pid);
+	// find task with the given pid
+	p = (pid != 0) ? find_task_by_vpid(pid) : &init_task;
 	if (p == NULL) {
-		printk(KERN_DEBUG "sched_getweight: process not found");
 		return -ESRCH;
 	}
 
+	// task's scheduling policy must be WRR
 	if (p->policy != SCHED_WRR) {
-		printk(KERN_DEBUG "sched_getweight: process policy is not SCHED_WRR");
 		return -EINVAL;
 	}
 
