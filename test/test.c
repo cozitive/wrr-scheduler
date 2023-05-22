4ac853e9945441e1fa515ce119c7d00941ac1e22
Hyein Kim <cozitive.dev@gmail.com>
2023-05-22 17:48:37 +0900

    run weighted test processes concurrently

--- test/test.c
+++ test/test.c
@@ -4,11 +4,14 @@
 #include <stdlib.h>
 #include <sched.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include "wrappers.h"
 
 #define X_DEFAULT 999999937
 #define MAX_WEIGHT 20
 #define TRIAL_COUNT 3
+
+double turnaround[MAX_WEIGHT];
 
 void factorize(int x);
 
@@ -16,36 +19,54 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
     int x = (argc > 1) ? rand() : X_DEFAULT;
-	int pid = getpid();
-    struct timeval start, end;
-	double turnaround[MAX_WEIGHT];
+
+	if (getuid() != 0) {
+		printf("WRR turnaround time test should be run in root user\n");
+		return 0;
+	}
 
 	printf("WRR turnaround time test: prime factoriazation of %d\n\n", x);
 
-	for (int weight = 1; weight <= MAX_WEIGHT; weight++) {
-		int i = weight - 1;
-		turnaround[i] = 0;
-		printf("========== weight <%d> ==========\n", weight);
-		sched_setweight(pid, weight);
-
-		for (int cnt = 0; cnt < TRIAL_COUNT; cnt++) {
-			gettimeofday(&start, NULL);
-			factorize(x);
-			gettimeofday(&end, NULL);
-
-			double elapsed = (double)(end.tv_usec - start.tv_usec) / 1000000 + (double)(end.tv_sec - start.tv_sec);
-			printf("\t[%d] %lf secs\n", cnt, elapsed);
-			turnaround[i] += elapsed;
+	pid_t pids[MAX_WEIGHT];
+	int is_child = 0;
+	int weight = 0;
+	for (int i = 0; i < MAX_WEIGHT; i++) {
+		pid_t pid = fork();
+		if (pid != 0) {
+			pids[i] = pid;
+		} else {
+			is_child = 1;
+			weight = i + 1;
+			break;	// stop fork in child process
 		}
-		printf("---------------------------------\n");
-		printf("sum: %lf secs\tavg: %lf secs\n\n", turnaround[i], turnaround[i] / TRIAL_COUNT);
 	}
 
-	FILE *f = fopen("out.txt", "w");
-	for (int i = 0; i < 20; i++) {
-		fprintf(f, "weight: %d\tsum: %lf\tavg: %lf\n", i + 1, turnaround[i], turnaround[i] / TRIAL_COUNT);
-	}	
-	fclose(f);
+	if (!is_child) {
+		for (int i = 0; i < MAX_WEIGHT; i++) {
+			sched_setweight(pids[i], i + 1);
+			// waitpid(pids[i], NULL, 0);	// wait for child process to terminate
+		}
+	} else {
+		struct timeval start, end;
+		double elapsed;
+		
+		while (sched_getweight(getpid()) != weight);
+
+		gettimeofday(&start, NULL);
+		factorize(x);
+		gettimeofday(&end, NULL);
+
+		elapsed = (double)(end.tv_usec - start.tv_usec) / 1000000 + (double)(end.tv_sec - start.tv_sec);
+		printf("[%d] weight %2d: %lf secs\n", weight, weight, elapsed);
+		turnaround[weight - 1] = elapsed;
+		return 0;
+	}
+
+	// FILE *f = fopen("out.txt", "w");
+	// for (int i = 0; i < 20; i++) {
+	// 	fprintf(f, "weight: %d\tsum: %lf\tavg: %lf\n", i + 1, turnaround[i], turnaround[i] / TRIAL_COUNT);
+	// }	
+	// fclose(f);
 
 	return 0;
 }
@@ -55,14 +76,14 @@ void factorize(int x)
 	int div = 2;
 	int is_first = 1;
 
-	printf("%d = ", x);
+	// printf("%d = ", x);
 	while (x > 1) {
 		if (x % div == 0) {
 			if (is_first) {
-				printf("%d", div);
+				// printf("%d", div);
 				is_first = 0;
 			} else {
-				printf(" * %d", div);
+				// printf(" * %d", div);
 			}
 			x /= div;
 		} else {
